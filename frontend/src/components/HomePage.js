06b05dee0e41128e3d40e2c5baeb6423f46fc665
Alexander Roberts <remidy@live.co.uk>
2021-01-08 23:38:45 +0000

    Created HomePage Render

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core'
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 export default class HomePage extends Component {
@@ -9,11 +10,34 @@ export default class HomePage extends Component {
         super(props);
     }
 
+    renderHomePage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" compact="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={Link}>
+                            Join A Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={Link}>
+                            Create A Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
     render() {
         return (
             <Router>
                 <switch>
-                    <Route exact path='/'><p>This is the home page</p>   </Route>
+                    <Route exact path='/'>{this.renderHomePage()}
+                    </Route>
                     <Route path='/join' component={RoomJoinPage} />
                     <Route path='/create' component={CreateRoomPage} />
                     {/* react router by default passes props to the room component that will have info relating to how we got there. It'll give a prop called match (how it matched the url string path) and that will give access to the params from the url. Grab room code from there. */}
